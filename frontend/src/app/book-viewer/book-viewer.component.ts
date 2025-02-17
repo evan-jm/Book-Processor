1bcccd7e4280897d93c9bad0bb8a5437fec7cac2
Evan Mitchell <ejm0214@gmail.com>
2025-02-17 13:56:53 -0500

    book viewer updates

--- frontend/src/app/book-viewer/book-viewer.component.ts
+++ frontend/src/app/book-viewer/book-viewer.component.ts
@@ -1,11 +1,17 @@
 import { Component, Input } from '@angular/core';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-book-viewer',
-  imports: [],
+  imports: [NgIf],
   templateUrl: './book-viewer.component.html',
   styleUrl: './book-viewer.component.scss'
 })
 export class BookViewerComponent {
   @Input() book: string[] = [];
+  public currentLine: number = 0;
+
+  public nextLine(): void {
+    this.currentLine++;
+  }
 }
